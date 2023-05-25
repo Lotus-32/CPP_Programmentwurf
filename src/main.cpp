a1b68ca9e6a0fa34e25eee040b742035f982ced5
Lotus-32 <tobias.skoberla@gmail.com>
2023-05-25 22:21:25 +0200

    Initialized Project with CMakefiles and Logging

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
+
+/**
+ * @brief Initializes the logging system
+ *
+ */
+void initLogging() {
+  // Laedt das Konfigurationsfile und konfiguriert die Logger
+  el::Configurations conf("assets/logging.ini");
+  // Schaltet Mechanismus aus der das Programm bei fatalem Error beendet
+  el::Loggers::addFlag(el::LoggingFlag::DisableApplicationAbortOnFatalLog);
+  // Bei Systemcrash wird der Crashgrund in der Log Datei noch hinterlegt
+  el::Loggers::addFlag(el::LoggingFlag::LogDetailedCrashReason);
+  // Falls Bildschirmausgabe, dann farbig
+  el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
+  // Uebertraegt die Einstellungen an die Logger die laufen
+  el::Loggers::reconfigureLogger("default", conf);
+  el::Loggers::reconfigureAllLoggers(conf);
+
+  // LOG(INFO) << "Infos zum Ablauf";
+  // LOG(DEBUG) << "Debugging von Programmteile";
+  // LOG(WARNING) << "Das Programm kann noch weiterlaufen";
+  // LOG(ERROR) << "Das ist schon ein richtig schwerwiegender Fehler";
+  // LOG(FATAL) << "Hier fehlt z.B. schon der Speicher um weiterzu machen";
+}
+
+int main(int argc, char** argv) {
+  initLogging();
+
+  return 0;
+}
