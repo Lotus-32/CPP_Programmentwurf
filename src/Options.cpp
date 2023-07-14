a3bb007a603569f64e327612ece4c0ac3e8ec19f
Lotus-32 <tobias.skoberla@gmail.com>
2023-07-14 22:47:08 +0200

    options doxygen added

--- src/Options.cpp
+++ src/Options.cpp
@@ -99,6 +99,17 @@ void Options::parseGlobaleOptions(int argc, char** argv) {
   }
 }
 
+/**
+ * The function `parseLocalOptions` parses a JSON string containing local
+ * options and sets the corresponding variables if they are not already set.
+ *
+ * @param locales The `locales` parameter is a string that contains
+ * JSON-formatted data representing various options for the program. These
+ * options include the output filename, output type, header directory, source
+ * directory, namespace, sign per line, and sort by variable name.
+ * @param inputFileName The `inputFileName` parameter is a string that
+ * represents the name of the input file.
+ */
 void Options::parseLocalOptions(const string& locales,
                                 const string& inputFileName) {
   Json::Value json;
@@ -145,64 +156,157 @@ void Options::parseLocalOptions(const string& locales,
   }
 }
 
+/**
+ * The function "getOutputFilename" returns the output filename as a string.
+ *
+ * @return The outputFilename is being returned.
+ */
 string Options::getOutputFilename() const { return outputFilename; }
 
+/**
+ * The function "getOutputType" returns the output type as a string.
+ *
+ * @return the value of the variable "outputType" as a string.
+ */
 string Options::getOutputType() const { return outputType; }
 
+/**
+ * The function "getHeaderDir" returns the value of the "headerDir" variable.
+ *
+ * @return the value of the member variable "headerDir", which is a string.
+ */
 string Options::getHeaderDir() const { return headerDir; }
 
+/**
+ * The function getSourceDir() returns the source directory.
+ *
+ * @return the value of the variable "sourceDir".
+ */
 string Options::getSourceDir() const { return sourceDir; }
 
+/**
+ * The function "getNamespace" returns the value of the namespaceStr variable.
+ *
+ * @return the value of the variable `namespaceStr`, which is a string.
+ */
 string Options::getNamespace() const { return namespaceStr; }
 
+/**
+ * The function returns the value of the signPerLine variable.
+ *
+ * @return The value of the member variable "signPerLine" is being returned.
+ */
 int Options::getSignPerLine() const { return signPerLine; }
 
+/**
+ * The function returns the value of the sortByVarName variable.
+ *
+ * @return the value of the member variable `sortByVarName`.
+ */
 bool Options::getSortByVarName() const { return sortByVarName; }
 
+/**
+ * The function returns a boolean value indicating whether the output filename
+ * has been set.
+ *
+ * @return the value of the variable "isSetOutputFilename".
+ */
 bool Options::getIsSetOutputFilename() const { return isSetOutputFilename; }
 
+/**
+ * The function sets the output filename if it has not been previously set.
+ *
+ * @param outputFilename The output filename is a string that represents the
+ * name of the file where the output will be written to.
+ */
 void Options::setOutputFilename(string outputFilename) {
   if (!isSetOutputFilename) {
     this->outputFilename = outputFilename;
   }
 }
 
+/**
+ * The function sets the output type if it has not been previously set.
+ *
+ * @param outputType The outputType parameter is a string that represents the
+ * type of output that needs to be set.
+ */
 void Options::setOutputType(string outputType) {
   if (!isSetOutputType) {
     this->outputType = outputType;
   }
 }
 
+/**
+ * The function "setHeaderDir" sets the header directory if it has not been
+ * previously set.
+ *
+ * @param headerDir The parameter "headerDir" is a string that represents the
+ * directory where header files are located.
+ */
 void Options::setHeaderDir(string headerDir) {
   if (!isSetHeaderDir) {
     this->headerDir = headerDir;
   }
 }
 
+/**
+ * The function "setSourceDir" sets the source directory if it has not been
+ * previously set.
+ *
+ * @param sourceDir The source directory that you want to set.
+ */
 void Options::setSourceDir(string sourceDir) {
   if (!isSetSourceDir) {
     this->sourceDir = sourceDir;
   }
 }
 
+/**
+ * The function "setNamespace" sets the namespace string if it has not been
+ * previously set.
+ *
+ * @param namespaceStr The parameter `namespaceStr` is a string that represents
+ * the namespace value that we want to set.
+ */
 void Options::setNamespace(string namespaceStr) {
   if (!isSetNamespace) {
     this->namespaceStr = namespaceStr;
   }
 }
 
+/**
+ * The function sets the number of signs per line if it has not already been
+ * set.
+ *
+ * @param signPerLine The parameter "signPerLine" is an integer that represents
+ * the number of signs per line.
+ */
 void Options::setSignPerLine(int signPerLine) {
   if (!isSetSignPerLine) {
     this->signPerLine = signPerLine;
   }
 }
 
+/**
+ * The function sets the value of the sortByVarName variable if it has not
+ * already been set.
+ *
+ * @param sortByVarName A boolean value indicating whether or not to sort by
+ * variable name.
+ */
 void Options::setSortByVarName(bool sortByVarName) {
   if (!isSetSortByVarName) {
     this->sortByVarName = sortByVarName;
   }
 }
 
+/**
+ * The function getFileNames() returns a vector of strings containing file
+ * names.
+ *
+ * @return A vector of strings containing file names.
+ */
 vector<string> Options::getFileNames() const { return fileNames; }
 
 void Options::printHelp() {
