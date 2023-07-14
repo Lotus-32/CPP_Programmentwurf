0707d39a3f5e2593675cc8b9cb2660f2f5e38352
Lotus-32 <tobias.skoberla@gmail.com>
2023-07-14 22:10:58 +0200

    main reviewed

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include <CCodegenerator.h>
 #include <CTextToCPP.h>
-#include <CTextToEscSeq.h>
-#include <CTextToHexSeq.h>
-#include <CTextToOctSeq.h>
-#include <CTextToRawHexSeq.h>
 #include <Options.h>
 #include <easylogging++.h>
-#include <getopt.h>
 #include <stdio.h>
 
 #include <filesystem>
@@ -23,56 +18,87 @@ INITIALIZE_EASYLOGGINGPP
  * This function initializes logging.
  */
 void initLogging() {
-  // Laedt das Konfigurationsfile und konfiguriert die Logger
+  // Load the configuration file and configure the loggers
   el::Configurations conf("assets/logging.ini");
-  // Schaltet Mechanismus aus der das Programm bei fatalem Error beendet
+  // Disable program termination on fatal error
   el::Loggers::addFlag(el::LoggingFlag::DisableApplicationAbortOnFatalLog);
-  // Bei Systemcrash wird der Crashgrund in der Log Datei noch hinterlegt
+  // Include detailed crash reason in the log file upon system crash
   el::Loggers::addFlag(el::LoggingFlag::LogDetailedCrashReason);
-  // Falls Bildschirmausgabe, dann farbig
+  // Enable colored terminal output if available
   el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
-  // Uebertraegt die Einstellungen an die Logger die laufen
+  // Apply the configurations to all running loggers
   el::Loggers::reconfigureLogger("default", conf);
   el::Loggers::reconfigureAllLoggers(conf);
 
-  // LOG(INFO) << "Infos zum Ablauf";
-  // LOG(DEBUG) << "Debugging von Programmteile";
-  // LOG(WARNING) << "Das Programm kann noch weiterlaufen";
-  // LOG(ERROR) << "Das ist schon ein richtig schwerwiegender Fehler";
-  // LOG(FATAL) << "Hier fehlt z.B. schon der Speicher um weiterzu machen";
+  // LOG(INFO) << "Information about the process";
+  // LOG(DEBUG) << "Debugging from program parts";
+  // LOG(WARNING) << "The program can still continue";
+  // LOG(ERROR) << "This is already a serious error";
+  // LOG(FATAL) << "For example, there is not enough memory to continue";
 }
 
+/**
+ * Creates a file with the given name and content in the specified path,
+ * handling any necessary directory creation.
+ *
+ * @param fileName The name of the file to be created.
+ * @param content The `content` that will be written to the file.
+ * @param path The `path` where the file should be created.
+ */
 void createFile(const string& fileName, const string& content,
                 const string& path) {
   string correctedPath = path;
   replace(correctedPath.begin(), correctedPath.end(), '\\', '/');
-  LOG(DEBUG) << "Pfad: " << correctedPath << endl;
+  LOG(DEBUG) << "Path: " << correctedPath << endl;
 
   if (!filesystem::exists(correctedPath)) {
     if (!filesystem::create_directories(correctedPath)) {
-      LOG(ERROR) << "Fehler beim Erstellen des Verzeichnisses: "
-                 << correctedPath << endl;
+      LOG(ERROR) << "Error creating the directory: " << correctedPath << endl;
+      cerr << "Error creating the directory: " << correctedPath << endl;
       exit(1);
     }
   }
 
   ofstream outputFile(correctedPath + "/" + fileName);
   if (!outputFile) {
-    LOG(ERROR) << "Fehler beim Erstellen der Datei: " << fileName << endl;
+    LOG(ERROR) << "Error creating the file: " << fileName << endl;
+    cerr << "Error creating the file: " << fileName << endl;
     exit(1);
   }
 
   outputFile << content;
 
-  LOG(INFO) << "Datei erstellt: " << correctedPath << fileName << endl;
+  LOG(INFO) << "File created: " << correctedPath << fileName << endl;
 }
 
+/**
+ * Takes a string as input and returns a new string with all characters
+ * converted to lowercase.
+ *
+ * @param input The input parameter is a constant reference to a string.
+ *
+ * @return a string that is the lowercase version of the input string.
+ */
 string toLowerCases(const string& input) {
   string output = input;
   transform(output.begin(), output.end(), output.begin(), ::tolower);
   return output;
 }
 
+/**
+ * The main function reads input files, processes their contents, generates C++
+ * code, and writes the generated code to output files.
+ *
+ * @param argc The parameter `argc` is an integer that represents the number of
+ * command-line arguments passed to the program.
+ * @param argv argv is an array of strings that represents the command-line
+ * arguments passed to the program. The first element (argv[0]) is the name of
+ * the program itself, and the following elements (argv[1], argv[2], etc.) are
+ * the arguments provided by the user when running the program.
+ *
+ * @return The main function is returning an integer value of 0 if everything
+ * was successful.
+ */
 int main(int argc, char** argv) {
   initLogging();
 
@@ -84,12 +110,13 @@ int main(int argc, char** argv) {
   // Start of Codegenerator ---------------------------------------------
 
   for (string file : files) {
-    LOG(INFO) << "Datei: " << file << endl;
+    LOG(INFO) << "File: " << file << endl;
 
-    // Inputdatei oeffnen und Inhalt lesen
+    // Open input file and read its contents
     ifstream inputFile(file);
     if (!inputFile) {
-      LOG(ERROR) << "Fehler beim Oeffnen der Datei: " << file << endl;
+      LOG(ERROR) << "Error opening the file: " << file << endl;
+      cerr << "Error opening the file: " << file << endl;
       continue;
     }
 
@@ -124,24 +151,8 @@ int main(int argc, char** argv) {
                localeOptions->getHeaderDir());
     createFile(localeOptions->getOutputFilename() + sourceType, implementation,
                localeOptions->getSourceDir());
-    // ----Testausgaben---------------------------------------------------
 
-    // LOG(INFO) << "Inhalt: \n" << textToCPP->writeDeclaration() << endl;
-    // textToCPP->sort();
-    // LOG(INFO) << "Inhalt nach sort: \n"
-    //           << textToCPP->writeDeclaration() << endl;
-    // textToCPP->addElement(new CTextToEscSeq("Test", "Hallo Welt",
-    //                                         localeOptions->getSignPerLine()));
-    // textToCPP->addElement(new CTextToHexSeq("Ann_den_Anfang", "Hallo Welt",
-    //                                         localeOptions->getSignPerLine()));
-    // textToCPP->sort();
-
-    // LOG(INFO) << "Declaration mit Namespace: \n" << declaration << endl;
-    // LOG(INFO) << "Implementierung mit Namespache: \n" << implementation <<
-    // endl;
-
-    // ----Ende:-Testausgaben---------------------------------------------------
-
+    // Clean up
     delete codegenerator;
     delete localeOptions;
     textToCPP->clear();
