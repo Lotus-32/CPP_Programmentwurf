8364a54863b90f54f4e82eaa24e2440a88c5a5a7
Lotus-32 <tobias.skoberla@gmail.com>
2023-07-14 15:56:53 +0200

    Deleted outputfiles

--- Testpfad/include/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TEST_
-#define _TEST_
-
-extern const char * const test;
-
-#endif
--- Testpfad/src/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <test.h>
-
-const char * const test = {
-"Irgend ein langer Text, der immer weiter geht.\nDann noch" \
-" eine neue Zeile. \n \nHier ist eine leer Zeile"
-};
